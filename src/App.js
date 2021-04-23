ac09011455816442afa3f9602e0ca882376f7065
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-04-23 16:43:03 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import "./App.css";
 
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
- 
-mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzYzk2IiwiYSI6ImNrbnUxOWI0dzA0eWEydnBpZGVyM2FmNXkifQ.E6T6V8OsRj_YsvvjDQV9AA';
-
-new mapboxgl.Map({
-container: 'byron_the_noob',
-style: 'mapbox://styles/mapbox/streets-v11'
-});
-
 function App() {
+  var mapboxgl = require("mapbox-gl/dist/mapbox-gl.js");
+
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoidGhvbWFzYzk2IiwiYSI6ImNrbnUxOWI0dzA0eWEydnBpZGVyM2FmNXkifQ.E6T6V8OsRj_YsvvjDQV9AA";
+
+  new mapboxgl.Map({
+    container: "byron_the_noob",
+    style: "mapbox://styles/mapbox/streets-v11",
+  });
   return (
     <div className="App">
       <link
         href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
         rel="stylesheet"
       />
-        <p>
-          byron dum
-        </p>
-        <div id="byron_the_noob">
-
-        </div>
+      <p>byron dum</p>
+      <div id="byron_the_noob"></div>
     </div>
   );
 }
