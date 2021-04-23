96ee0ed2f0a4b0f05ffec9ed4e55ffc64173805e
ThomasJamesCrawford <ThomasJamesCrawford@gmail.com>
2021-04-23 16:29:52 +0800

    lol

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzYzk2IiwiYSI6ImNrbnUxOWI0dzA0eWEydnBpZGVyM2FmNXkifQ.E6T6V8OsRj_YsvvjDQV9AA';
+var map = new mapboxgl.Map({
+container: 'byron_the_noob',
+style: 'mapbox://styles/mapbox/streets-v11'
+});
 
 function App() {
   return (
     <div className="App">
+      <link
+        href="https://api.mapbox.com/mapbox-gl-js/v2.1.1/mapbox-gl.css"
+        rel="stylesheet"
+      />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div id="byron_the_noob">
+
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
